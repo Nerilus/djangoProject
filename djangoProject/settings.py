10e713b79b806d3f5cc8d5b6bd319a81c50c4cbb
nerilus herby <nerilus.h@gmail.com>
2024-07-26 16:47:32 +0200

    update settings

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-wd56r$o^4ygf+v^&ijxbrvu2gaedqhlc3a68j!x(o!!!+39f71
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['62.72.16.216', 'localhost', '127.0.0.1']
 
 
 # Application definition
