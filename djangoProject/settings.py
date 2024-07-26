0365fb0cf078dcd77cbcbbd589596fb6399a93da
nerilus herby <nerilus.h@gmail.com>
2024-07-26 17:13:27 +0200

    update settings

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -152,7 +152,11 @@ SIMPLE_JWT = {
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://62.72.16.216:8000",
+    "http://localhost:8000",
+]
+
 
 AUTH_USER_MODEL = 'djangoapp.User'
 
